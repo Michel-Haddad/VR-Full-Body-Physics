1892797818847897217b8baf3379f2c0b2436e79
Michel-Haddad <michohaddad80@gmail.com>
2023-03-18 20:40:39 +0200

    Set up first basic scene

---
+++ Assets/FireBullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FireBullet : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullets);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullets(ActivateEventArgs arg)
+    {
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = spawnPoint.position;
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnedBullet, 5);
+    }
+}
